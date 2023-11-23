110fa1d1821ac218656e54bcdf2c6ffc3ab005a5
root <root@udalovs.ru>
2023-11-23 09:53:40 +0000

    init

---
+++ lib/dokku/monitor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "monitor/version"
+
+module Dokku
+  module Monitor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
